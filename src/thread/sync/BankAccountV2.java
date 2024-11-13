84dc96e006c1d62e09fea3b734a3c6206438a81f
onepunchkim <hyukjin4711@gmail.com>
2024-11-13 18:30:51 +0900

    sync study

--- src/thread/sync/BankAccountV2.java
+++ src/thread/sync/BankAccountV2.java
@@ -14,6 +14,7 @@ public class BankAccountV2 implements BankAccount{
     public synchronized boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
 
+        // ==임계 영역 시작==
         log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
         if (balance < amount) {
             log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
@@ -23,6 +24,7 @@ public class BankAccountV2 implements BankAccount{
         sleep(1000); //출금에 걸리는 시간으로 가정
         balance = balance - amount;
         log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
+        // ==임계 영역 종료==
 
         log("거래 종료");
         return true;
