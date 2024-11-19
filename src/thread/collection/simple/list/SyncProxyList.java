c0bebb3a15c9a1000e18941984debb098bb1a0f9
onepunchkim <hyukjin4711@gmail.com>
2024-11-19 18:40:44 +0900

    collection study

--- src/thread/collection/simple/list/SyncProxyList.java
+++ src/thread/collection/simple/list/SyncProxyList.java
@@ -8,9 +8,13 @@ public class SyncProxyList implements SimpleList{
         this.target = target;
     }
 
+    // 1. lock 획득
     @Override
     public synchronized void add(Object e) {
+        // 2. 원본 메서드 호출
         target.add(e);
+        // 3. 원본 메서드 반납
+        // 4. lock 반납
     }
 
     @Override
